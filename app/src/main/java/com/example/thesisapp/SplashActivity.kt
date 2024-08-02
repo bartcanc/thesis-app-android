b0044c4a117b1e7fc2bd162d7420f77f0a3f9c61
bartcanc <bartekr2002@gmail.com>
2024-08-02 11:14:26 +0200

    added a splash screen

--- app/src/main/java/com/example/thesisapp/SplashActivity.kt
+++ app/src/main/java/com/example/thesisapp/SplashActivity.kt
@@ -9,7 +9,6 @@ import pl.droidsonroids.gif.GifImageView
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
